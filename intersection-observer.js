71b3be1ff761041662ec75135eb5301c6786e4ae
zefirez <tsumirez@zoho.com>
2023-01-21 00:01:40 +0100

    Now observer removes itself after doing initial intersection interaction

--- intersection-observer.js
+++ intersection-observer.js
@@ -4,6 +4,7 @@ const observer = new IntersectionObserver(
     entries => {
         entries.forEach(entry => {
             entry.target.classList.toggle("show", entry.isIntersecting)
+            if(entry.isIntersecting) observer.unobserve(entry.target);
         })
     }, 
     {
