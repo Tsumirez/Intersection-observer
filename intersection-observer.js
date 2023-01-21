f3a673e80d1feab573770499621d3fe1f3fa2499
zefirez <tsumirez@zoho.com>
2023-01-21 09:23:23 +0100

    Addes observer options - root, rootMargin and treshold

--- intersection-observer.js
+++ intersection-observer.js
@@ -1,14 +1,18 @@
 const cards = document.querySelectorAll(".card");
+const cardContainer = document.getElementsByClassName("card-container")[0];
+
 
 const observer = new IntersectionObserver(
     entries => {
         entries.forEach(entry => {
             entry.target.classList.toggle("show", entry.isIntersecting)
-            if(entry.isIntersecting) observer.unobserve(entry.target);
+            // if(entry.isIntersecting) observer.unobserve(entry.target);
         })
     }, 
     {
-        threshold: 1
+        root: cardContainer,
+        threshold: 1,
+        rootMargin: "-0px"
     }
 )
 
